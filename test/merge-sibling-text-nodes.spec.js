a1e927ee414032386446f5e20fb07791fa3c3fc7
raffa-sh <rafael.shayvolodyan@synthasite.com>
2018-11-27 12:34:07 +0200

    Fix mergeSiblingTextNodes tests

--- test/merge-sibling-text-nodes.spec.js
+++ test/merge-sibling-text-nodes.spec.js
@@ -23,7 +23,7 @@ describe('mergeSiblingTextNodes', () => {
   });
   describe('when node is not a TextNode', () => {
     it('should throw a TypeError', () => {
-      expect(() => mergeSiblingTextNodes(document.createElement('div'))).toThrowError(TypeError);
+      expect(mergeSiblingTextNodes(document.createElement('div'))).toThrowError(TypeError);
     });
   });
 });
